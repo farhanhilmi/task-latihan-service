fb8f1bbe6df0a3d5db52a6f82a34471c49390c1c
Farhan Hilmi <farhanhilmi077@gmail.com>
2022-02-25 15:39:57 +0700

    add user routes

---
+++ src/Routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import handler from '../Handler/userHandler.js';
+
+const userRoutes = () => {
+  const router = express.Router();
+
+  router.post('/', handler.postUser);
+  router.get('/', handler.getUsers);
+  router.get('/:userId', handler.getById);
+  router.put('/:userId', handler.updateUser);
+  router.delete('/:userId', handler.deleteUserById);
+
+  return router;
+};
+
+export default userRoutes;
